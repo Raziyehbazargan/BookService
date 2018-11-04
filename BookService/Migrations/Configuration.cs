c5cdf51a144f26048757649dea0babf2772bf8fa
Raziyeh Bazargan <raziyeh.bazargan@gmail.com>
2018-11-03 20:11:41 -0700

    Add API controller + enable migration

---
+++ BookService/Migrations/Configuration.cs
@@ -0,0 +1,76 @@
+namespace BookService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using BookService.Models;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BookService.Models.BookServiceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BookService.Models.BookServiceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            context.Authors.AddOrUpdate(x => x.Id,
+                new Author() { Id = 1, Name = "Jane Austen" },
+                new Author() { Id = 2, Name = "Charles Dickens" },
+                new Author() { Id = 3, Name = "Miguel de Cervantes" }
+            );
+
+            context.Books.AddOrUpdate(x => x.Id,
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Pride and Prejudice",
+                    Year = 1813,
+                    AuthorId = 1,
+                    Price = 9.99M,
+                    Genre = "Comedy of manners"
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Northanger Abbey",
+                    Year = 1817,
+                    AuthorId = 1,
+                    Price = 12.95M,
+                    Genre = "Gothic parody"
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Title = "David Copperfield",
+                    Year = 1850,
+                    AuthorId = 2,
+                    Price = 15,
+                    Genre = "Bildungsroman"
+                },
+                new Book()
+                {
+                    Id = 4,
+                    Title = "Don Quixote",
+                    Year = 1617,
+                    AuthorId = 3,
+                    Price = 8.95M,
+                    Genre = "Picaresque"
+                }
+            );
+        }
+    }
+}
